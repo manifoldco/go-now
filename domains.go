26a9da5bd01f4745f4e1802f2adf421c5796ef97
Jeff Andersen <jeffandersen@gmail.com>
2017-08-26 20:34:53 -0300

    Add domain name field

--- domains.go
+++ domains.go
@@ -5,6 +5,7 @@ import "time"
 // Domain is the contents of a domain object
 type Domain struct {
 	UID         string     `json:"uid"`
+	Name        string     `json:"name"`
 	Verified    bool       `json:"verified"`
 	VerifyToken string     `json:"verifyToken,omitempty"`
 	Created     *time.Time `json:"created,omitempty"`
